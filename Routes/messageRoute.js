fe9eb76f0a1326f7c2aca4bca839611573094eb0
sanahiravp0 <sanahiravpo@gmail.com>
2025-04-08 01:28:57 +0530

    Initial commit or update code

--- Routes/messageRoute.js
+++ Routes/messageRoute.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { createMessage, getMessages ,getMessagesBetweenUsers} from '../Controller/messageController.js';
+import { createMessage, getMessages, getMessagesBetweenUsers } from '../Controller/messageController.js';
 
 const router = express.Router();
 
 router.get('/', getMessages);
-router.post('/', createMessage)
-router.get('/:user1/:user2', getMessagesBetweenUsers);
+router.post('/', createMessage);
 
+// ✅ Fix: add descriptive path
+router.get('/between/:user1/:user2', getMessagesBetweenUsers);
 
 export default router;
